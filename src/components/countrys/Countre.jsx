0fe09b47dd5fbd9101cd17905f31dd862b360153
Md. Rony <ronimdrahim467@gmail.com>
2025-09-25 01:56:39 +0600

    Ak jsx theke ar jsx e Props pathano ba Child Class thke parent Class e Child Pathano

--- src/components/countrys/Countre.jsx
+++ src/components/countrys/Countre.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Countrys.css'
 
-const countre = ({countre}) => {
-      const boxStyle = {
-        padding:'20px',
-        textAlign:"center",
-        border: "2px solid greenyellow",
-    };
+const countre = ({countre,handelVisitedCountrys}) => {
+     
      const currencyCode = Object.keys(countre.currencies.currencies)[0]; 
+     // eslint-disable-next-line react-hooks/rules-of-hooks
+     const [Visited, setVisited]=useState(false);
+
+     const handleVisited=()=>{
+        //Besic System-----
+        // if(Visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        //Second System----
+        // setVisited(Visited ? false :true)
+
+        //Advance And Most Use System---
+        setVisited(!Visited)
+        handelVisitedCountrys(countre)
+     }
     return (
-        <div style={boxStyle}>
+        <div className={`visited ${Visited ? 'visited-color' :"boxStyle"}`}>
             <img src={countre.flags.flags.png} alt="" />
             <h3>Name : {countre.name.common}</h3>
             <h3>Capital : {countre.capital.capital[0]}</h3>
             <h4>Region :  {countre.region.region}</h4>
-            <h4>Currence : {currencyCode}</h4>
+            <p>Currence : {currencyCode}</p>
+              <p>Area : { countre.area.area>300000 ? "Big Country" : "Small Country"}</p>
+              <button onClick={handleVisited}>
+                {
+                    Visited ? "Visited" : "Not Visited"
+                }
+              </button>
         </div>
     );
 };
--- src/components/countrys/countrys.jsx
+++ src/components/countrys/countrys.jsx
@@ -1,24 +1,34 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Countre from './Countre';
 import './Countrys.css';
 
 const Countrys = ({ countresPromise }) => {
+    const [visitedCountrys, setVisitedCountrys]=useState([]);
+    const handelVisitedCountrys=(countre)=>{
+        console.log("visited Country is cliked",countre)
+    }
     
     const countresData = use(countresPromise);
     const AllCountres = countresData.countries;
     console.log(AllCountres);
 
+      const headStyle = {
+        textAlign: "center",
+        margin: "0 auto",
+        padding: "10px 0",
+        color: "White   "
+    };
+
     return (
         <div>
-            <h2>World On The Go : {AllCountres.length}</h2>
+            <h2 style={headStyle}>World On The Go : {AllCountres.length}</h2>
+            <h3>Total Visited Country : </h3>
             <div className="countrys-grid">
                 {
-                    AllCountres.map(countre => (
-                        <Countre 
-                            countre={countre}
-                            key={countre.cca3.cca3}
-                        />
-                    ))
+                    AllCountres.map(countre => <Countre
+                      countre={countre}
+                         key={countre.cca3.cca3}
+                         handelVisitedCountrys={handelVisitedCountrys}></Countre>)
                 }
             </div>
         </div>
